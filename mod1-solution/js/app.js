15f50ea5a8434f980395d727d0abb81995edc8e1
Auwalms <auwalforall@gmail.com>
2017-01-05 16:24:44 +0100

    working on the module

--- mod1-solution/js/app.js
+++ mod1-solution/js/app.js
@@ -10,10 +10,17 @@
 
     LunchCheckCtrl.$inject = ['$scope'];
     function LunchCheckCtrl($scope) {
+      var holder = []; //holds strings after the are split
+      $scope.foodList = ''; //gets content of the textbox.
+      holder.push($scope.foodList);
 
 
       $scope.checkStatus = function () {
-        
+        if(holder.length !== 0){
+          console.log("Not Zero");
+        }else {
+          console.log("Zero!");
+        }
       }
 
     }
