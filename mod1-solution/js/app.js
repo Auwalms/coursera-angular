878440439dfb0433495850eead32fb6da64ac433
Auwalms <auwalforall@gmail.com>
2017-01-05 18:25:42 +0100

    logic added. minor update needed

--- mod1-solution/js/app.js
+++ mod1-solution/js/app.js
@@ -1,6 +1,4 @@
 // This will house the app logic.
-
-
 (
   function() {
     'use strict';//make sure no unforseen error is set loose
@@ -10,17 +8,19 @@
 
     LunchCheckCtrl.$inject = ['$scope'];
     function LunchCheckCtrl($scope) {
-      var holder = []; //holds strings after the are split
       $scope.foodList = ''; //gets content of the textbox.
-      holder.push($scope.foodList);
 
 
       $scope.checkStatus = function () {
+      $scope.splited = $scope.foodList.split(',');
+      console.log($scope.splited.length);
+        /**
         if(holder.length !== 0){
           console.log("Not Zero");
         }else {
           console.log("Zero!");
         }
+        **/
       }
 
     }
