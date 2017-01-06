79a184a83dc32d6c768b7efc90865f35ee1f9a24
Auwalms <auwalforall@gmail.com>
2017-01-06 21:46:39 +0100

    done with assignment one

--- mod1-solution/js/app.js
+++ mod1-solution/js/app.js
@@ -4,21 +4,25 @@
     'use strict';//make sure no unforseen error is set loose
 
     var LunchCheck = angular.module('LunchCheck',[]);
-    LunchCheck.controller('LunchCheckCtrl', LunchCheckCtrl);
+    LunchCheck.controller('LunchCheckController', LunchCheckController);
 
-    LunchCheckCtrl.$inject = ['$scope'];
-    function LunchCheckCtrl($scope) {
+    LunchCheckController.$inject = ['$scope'];
+    function LunchCheckController($scope) {
       $scope.foodList = ''; //gets content of the textbox.
 
 
       $scope.checkStatus = function () {
-        if ($scope.show == $scope.show.length) {
-            $scope.show = $scope.foodList;
-            console.log('The length of foodList is'+ $scope.show.length);
+        console.log($scope.foodList.length);
+        if ($scope.foodList.length < 1 ) {
+            $scope.show = true;
+            console.log('The length of foodList is'+ $scope.foodList);
+        }else {
+          $scope.show = false;
         }
 
       $scope.splited = $scope.foodList.split(',');
-      console.log($scope.splited.length);
+      console.log($scope.splited);
+
         /**
         if(holder.length !== 0){
           console.log("Not Zero");
