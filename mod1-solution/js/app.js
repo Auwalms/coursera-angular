124c197e60eaba13715b67bcf7821aa30a63d77b
Auwalms <auwalforall@gmail.com>
2017-01-06 21:55:01 +0100

    minot modification

--- mod1-solution/js/app.js
+++ mod1-solution/js/app.js
@@ -12,16 +12,14 @@
 
 
       $scope.checkStatus = function () {
-        console.log($scope.foodList.length);
         if ($scope.foodList.length < 1 ) {
             $scope.show = true;
-            console.log('The length of foodList is'+ $scope.foodList);
         }else {
           $scope.show = false;
         }
 
       $scope.splited = $scope.foodList.split(',');
-      console.log($scope.splited);
+
 
         /**
         if(holder.length !== 0){
