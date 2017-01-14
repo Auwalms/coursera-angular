80ccd6f26a1e4280e2dc5d42306346e64b43b318
Auwalms <auwalforall@gmail.com>
2017-01-14 22:50:57 +0100

    assignment functionality completed

--- mod2-solution/js/app.js
+++ mod2-solution/js/app.js
@@ -1,22 +1,53 @@
 ( function () {
   'use strict';
+
   angular.module('ShoppingListCheckOff', [])
         .controller('ToBuyController', ToBuyController)
         .controller('AlreadyBoughtController', AlreadyBoughtController)
-        ;
+        .service('ShoppingListCheckOffService',ShoppingListCheckOffService);
 
 
-
-function ToBuyController() {
+ToBuyController.$inject = ['ShoppingListCheckOffService'];
+// Declaring ToBuyController and injecting service.
+function ToBuyController(ShoppingListCheckOffService) {
   var toBuy = this;
- toBuy.text = 'ToBuyController Up and Ready!';
-};
+  toBuy.availableItems = ShoppingListCheckOffService.getAvailableItems();
 
-function AlreadyBoughtController() {
+  toBuy.buyItem = function (itemindex) {
+    ShoppingListCheckOffService.buyItem(itemindex);
+    console.log(toBuy.availableItems);
+  };
+
+}
+
+// Declaring AlreadyBoughtController and injecting service,
+AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+function AlreadyBoughtController(ShoppingListCheckOffService) {
   var alreadyBought = this;
-    alreadyBought.text = 'AlreadyBoughtController Up and Ready!';
+    alreadyBought.boughtItems = ShoppingListCheckOffService.getBoughtItems();
 
 }
+
+function ShoppingListCheckOffService() {
+  var service = this;
+//array containing items available in the store. -_-
+  var availableItems = [{ name: "cookies", quantity: 10 },{ name: "sugaries", quantity: 5 },{ name: "chocolates", quantity: 5},{ name: "candys", quantity: 15 },{ name: "bread", quantity: 10 }];
+  var boughtItems = []; //Empty array serving as basket for storing items bought
+
+  //function for buying of item triggered by item click.
+  service.buyItem = function (itemindex) {
+    var items = availableItems[itemindex];
+    availableItems.splice(itemindex,1);
+    boughtItems.push(itemindex);
+  };
+
+  service.getAvailableItems = function () {
+    return availableItems;
+  };
+  //function to check show items already in the basket
+  service.getBoughtItems= function () {
+    return boughtItems;
+  };
 }
 
-)()
+})()
