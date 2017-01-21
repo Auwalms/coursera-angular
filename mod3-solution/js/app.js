d94722038e7a302abbedc90981731182e5b8c403
Auwalms <auwalforall@gmail.com>
2017-01-22 00:13:25 +0100

    code functional. updating UI remaining

--- mod3-solution/js/app.js
+++ mod3-solution/js/app.js
@@ -1,45 +1,74 @@
 (function () {
-  'use strict';
+'use strict';
+
+angular.module('NarrowItDownApp', [])
+.controller('NarrowItDownController', NarrowItDownController)
+.service('MenuSearchService', MenuSearchService)
+.directive('foundItems', FoundItemsDirective)
+.constant('ApiPath', "http://davids-restaurant.herokuapp.com");
 
 
-  angular.module('NarrowItDownApp', [])
-         .controller('NarrowItDownController', NarrowItDownController)
-         .service('MenuSearchService', MenuSearchService)
-         .constant('ApiPath', "http://davids-restaurant.herokuapp.com");
-         //.directive('foundItems', foundItems);
+NarrowItDownController.$inject = ['MenuSearchService'];
+function NarrowItDownController(MenuSearchService) {
+  var list = this;
+  list.removeItem=function (itemPosition) {
+    list.found.splice(itemPosition, 1);
+  };
+
+  list.check=function () {
+      var promise = MenuSearchService.getMatchedMenuItems();
+      promise.then(function (response) {
+        var message = response.data.menu_items;
+        console.log(response.data);
+        list.found=[];
+        for (var i = 0; i < message.length; i++) {
+          if(message[i].description.indexOf(list.Val)!=-1)
+            list.found.push(message[i]);
+        }
+
+       })
+       .catch(function (error) {
+         console.error(error);
+       })
+      };
+
+}
 
 
-         NarrowItDownController.$inject=['MenuSearchService']
-         function NarrowItDownController(MenuSearchService) {
-           var narrowDownCtrl = this;
+MenuSearchService.$inject = ['$http','ApiPath'];
+function MenuSearchService($http,ApiPath) {
+  var service = this;
+  service.getMatchedMenuItems = function() {
+    return $http({
+        method:"GET",
+        url: (ApiPath +"/menu_items.json")
+    });
+  };
+}
+
+function FoundItemsDirective() {
+  var dir = {
+  templateUrl: 'foundItems.html',
+  scope: {
+    found:'<',
+    onRemove: '&'
+  },
+  controller: FoundItemsDirectiveController,
+  controllerAs: 'FIDCtrl',
+  bindToController: true
+
+  };
+  return dir;
+  }
+
+function FoundItemsDirectiveController() {
+  var FIDCtrl = this;
+
+  FIDCtrl.checkList = function () {
+    if(FIDCtrl.found===undefined || FIDCtrl.found.length===0)
+      return true;
+  };
+}
 
 
-
-         };//End of NarrowItDownController
-
-         MenuSearchService.$inject = ['$http','ApiPath'];
-         function MenuSearchService($http,ApiPath) {
-           var service = this;
-
-           service.getMatchedMenuItems = function (searchTerm) {
-             var response = $http({
-               method: "GET",
-               url: (ApiBasePath + "/menu_items.json"),
-               params: {
-                 category: searchTerm
-               }
-              });
-             return response;
-           };
-
-         };//End of MenuSearchService
-
-         function foundItems() {
-
-         };//End of foundItems
-
-
-
-
-
-})()
+})();
