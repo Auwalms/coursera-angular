5ad243ae7f847d6cfa2a25a1409a1943ca790e95
Auwalms <auwalforall@gmail.com>
2017-01-29 21:39:14 +0100

    additional updates

--- mod4-solution/js/components/categories.component.js
+++ mod4-solution/js/components/categories.component.js
@@ -1,17 +1,11 @@
 (function() {
   "use strict";
-  
+
   angular.module("MenuApp")
     .component("categories", {
       templateUrl: "js/components/categories.template.html",
-      controller: CategoriesComponentController,
       bindings: {
         items: "<"
       }
     });
-
-  CategoriesComponentController.$inject = [];
-  function CategoriesComponentController() {
-    // var comp = this;
-  }
 })();
